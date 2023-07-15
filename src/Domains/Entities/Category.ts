db7d5f6790591c94a85bf45e15cb50df0a00144a
kelwinv <kelwin08@gmail.com>
2023-07-15 18:49:06 -0300

    test(Category): teste Entity

--- src/Domains/Entities/Category.ts
+++ src/Domains/Entities/Category.ts
@@ -1,9 +1,16 @@
+import { UUIDGenerator } from "./UUIDGenerator";
+
 class Category {
   constructor(
     readonly id: string,
     readonly name: string,
     readonly description: string,
   ) {}
+
+  static create({ name, description }: { name: string; description: string }) {
+    const id = UUIDGenerator.create();
+    return new Category(id, name, description);
+  }
 }
 
 export { Category };
