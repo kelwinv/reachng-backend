d63afb44572a0d61b963fdb12b24598e066eb6ce
kelwinv <kelwin08@gmail.com>
2023-07-15 18:49:53 -0300

    test(Contact): teste Entity

--- src/Domains/Entities/Contact.ts
+++ src/Domains/Entities/Contact.ts
@@ -1,5 +1,6 @@
+import { UUIDGenerator } from "./UUIDGenerator";
+
 class Contact {
-  private static readonly EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   private static readonly VALID_TYPES = ["mail", "phone", "social"];
 
   constructor(
@@ -13,16 +14,40 @@ class Contact {
 
   private static isValidType(type: string): void {
     if (!Contact.VALID_TYPES.includes(type)) {
-      throw new Error("Invalid contact type");
+      throw new Error(`Invalid contact type: ${type}`);
     }
   }
 
   private static isValidFormat(type: string, value: string): void {
     if (type === "mail") {
-      if (!Contact.EMAIL_REGEX.test(value)) {
-        throw new Error("Invalid contact value format");
+      const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!EMAIL_REGEX.test(value)) {
+        throw new Error("Invalid mail contact value format");
       }
     }
+
+    if (type === "phone") {
+      const cleanedValue = value.replace(/\D/g, "");
+      const phoneRegex = /^\d{11}$/;
+      if (!phoneRegex.test(cleanedValue)) {
+        throw new Error("Invalid phone contact value format");
+      }
+      value = cleanedValue;
+    }
+
+    if (type === "social") {
+      const urlRegex = /^(https?:\/\/)?[^\s/$.?#].[^\s]*$/;
+
+      if (!urlRegex.test(value)) {
+        throw new Error("Invalid social contact value format");
+      }
+    }
+  }
+
+  static create({ type, value }: { type: string; value: string }) {
+    const id = UUIDGenerator.create();
+
+    return new Contact(id, type, value);
   }
 }
 
